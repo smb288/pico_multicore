c9a87a2ec31d3b6aecb4fcf15d0d4f4fa164832f
Sean Baker <sbaker101@gmail.com>
2021-10-28 14:07:27 -0500

    Dual Core Blinking LED

---
+++ multicore.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "hardware/uart.h"
+
+
+const int LED1_PIN = 0, 
+          LED2_PIN = 1;
+
+void pinInit(uint firstLED, uint secondLED) {
+    gpio_init(firstLED);
+    gpio_init(secondLED);
+    gpio_set_dir(firstLED, GPIO_OUT);
+    gpio_set_dir(secondLED, GPIO_OUT);
+}
+
+void secondCoreCode() {
+    while(1) {
+        gpio_put(LED1_PIN, 1);
+        sleep_ms(1000);
+        gpio_put(LED1_PIN, 0);
+        sleep_ms(1000);
+    }
+}
+
+int main() {
+
+    //Initialize pin numbers
+    pinInit(LED1_PIN, LED2_PIN);
+
+    //Launch second core
+    multicore_launch_core1(secondCoreCode);
+
+    while(1) {
+        gpio_put(LED2_PIN, 0);
+        sleep_ms(500);
+        gpio_put(LED2_PIN, 1);
+        sleep_ms(500);
+    }
+}
